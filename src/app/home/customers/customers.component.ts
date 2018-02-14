484920fd0882f251f79411a7f0b26b9c8bcb5944
jbernal <j.bernal@upm.es>
2018-02-14 19:43:05 +0100

    add table & resource

--- src/app/home/customers/customers.component.ts
+++ src/app/home/customers/customers.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { User } from '../cashier-opened/user.model';
+import { UserService } from '../cashier-opened/user.service';
 
 @Component({
     templateUrl: `customers.component.html`
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
     static URL = 'customers';
+
+    displayedColumns = ['mobile', 'username', 'email', 'actions'];
+    dataSource: MatTableDataSource<User>;
+
+    @ViewChild(MatSort) sort: MatSort;
+
+    constructor(private userService: UserService) {
+    }
+
+    ngOnInit(): void {
+        this.userService.readAll().subscribe(
+            data => {
+                this.dataSource = new MatTableDataSource<User>(data);
+                this.dataSource.sort = this.sort;
+            }
+        );
+    }
+
 }
