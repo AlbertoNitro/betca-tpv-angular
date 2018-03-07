42b81b96db4c6e1f955fb84ddb6534ecc2f52c95
angel <bo0477@192.168.7.19>
2018-03-07 15:27:11 +0100

    Voucher view. Routing

---
+++ src/app/home/vouchers/vouchers.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatDialog } from '@angular/material';
+
+@Component({
+    templateUrl: `vouchers.component.html`
+})
+export class VouchersComponent implements OnInit {
+    static URL = 'vouchers';
+
+    displayedColumns = ['mobile', 'username', 'actions'];
+
+    ngOnInit(): void {
+    }
+
+    
+}
