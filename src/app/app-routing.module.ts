cd47cae6840fa63aec32c233ca1afa2fad3775d2
borjaguzmanlopez94 <borja.guzman.lopez@alumnos.upm.es>
2018-03-11 11:56:54 +0100

    Introduzco el nuevo componente para estadisticas2 en el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CancelYesDialogComponent } from './core/cancel-yes-dialog.component';
-import {DbSeedDialogComponent } from './home/admin/db-seed-dialog.component';
+import { DbSeedDialogComponent } from './home/admin/db-seed-dialog.component';
 import { HomeComponent } from './home/home.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AdvancedSearchComponent } from './home/cashier-opened/advanced-search.component';
@@ -22,6 +22,7 @@ import { UsersComponent } from './home/users/users.component';
 import { UserCreationEditDialogComponent } from './home/users/user-creation-edit-dialog.component';
 import { VouchersComponent } from './home/vouchers/vouchers.component';
 import { ArticlesComponent } from './home/articles/articles.component';
+import { Statistics2Component } from './home/statistics2/statistics2.component';
 
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: WelcomeComponent.URL },
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
       { path: TicketsComponent.URL, component: TicketsComponent },
       { path: ArticlesComponent.URL, component: ArticlesComponent },
       { path: UsersComponent.URL, component: UsersComponent },
-      { path: VouchersComponent.URL, component: VouchersComponent }
+      { path: VouchersComponent.URL, component: VouchersComponent },
+      { path: Statistics2Component.URL, component: Statistics2Component }
     ]
   }
 ];
@@ -58,7 +60,8 @@ export class AppRoutingModule {
     TicketsComponent,
     UsersComponent,
     VouchersComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    Statistics2Component
   ];
 
   static COMPONENT_FACTORY = [
---
+++ src/app/home/statistics2/statistics2.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+
+
+@Component({
+    templateUrl: `statistics2.component.html`,
+    styleUrls: [`statistics2.component.css`],
+    providers: []
+})
+export class Statistics2Component implements OnInit {
+    static URL = 'statistics2';
+    constructor() {
+    }
+
+    ngOnInit(): void {
+
+    }
+
+}
