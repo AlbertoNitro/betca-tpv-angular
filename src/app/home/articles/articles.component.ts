14fef72b15d23c41c04e8ca43d8b6b9b0b030156
Chinegua <ai.chinegua@gmail.com>
2018-03-11 13:18:08 +0100

    adding table

--- src/app/home/articles/articles.component.ts
+++ src/app/home/articles/articles.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../shared/article.model';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
+
+
+
 export class ArticlesComponent implements OnInit {
   static URL = 'articles';
-  constructor() { }
+  private articleList: Article[] = [];
+  dataSource: MatTableDataSource<Article>;
+  displayedColumns = ['code', 'description', 'reference', 'retailprice', 'stock'];
+  constructor() {
+    this.articleList.push({ code: '12345', description: 'holaholahola', reference: '288', retailPrice: 200, stock: 2 });
+    this.articleList.push({ code: '12345', description: 'holaholahola', reference: '288', retailPrice: 200, stock: 2 });
+    this.articleList.push({ code: '12345', description: 'holaholahola', reference: '288', retailPrice: 200, stock: 2 });
+    this.articleList.push({ code: '12345', description: 'holaholahola', reference: '288', retailPrice: 200, stock: 2 });
+    this.dataSource = new MatTableDataSource<Article>(this.articleList);
+  }
 
   ngOnInit() {
   }
