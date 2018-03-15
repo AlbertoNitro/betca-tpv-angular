e94d37d11e7f7da46bc49ee34d0e853840f1dc72
bo0414 <isragoo.prez@gmail.com>
2018-03-15 10:56:45 +0100

    Fin Tarea 3 Add Método getAllArticles()

--- src/app/home/cashier-opened/articles-family/articles-family.component.ts
+++ src/app/home/cashier-opened/articles-family/articles-family.component.ts
@@ -14,11 +14,11 @@ import {ArticleService} from '../../shared/article.service';
 
 export class ArticlesFamilyComponent implements OnInit {
   static URL = 'articlesfamily';
-  positionabove = 'above';
-  positionleft = 'left';
-  imagePathArticle = '../../../assets/img/articles/art-blue.jpg';
-  imagePathFamily = '../../../assets/img/articles/folder-blue.png';
-
+  private positionabove = 'above';
+  private positionleft = 'left';
+  private imagePathArticle = '../../../assets/img/articles/art-blue.jpg';
+  private imagePathFamily = '../../../assets/img/articles/folder-blue.png';
+  private articleList: Article[] = [];
   listArt: Article[] = [
     {code: '111', reference: 'Article11', description: 'Article11 The titles of Washed', retailPrice: 81, stock: 156},
     {code: '211', reference: 'Article12', description: 'Article12 The titles of Washed', retailPrice: 26, stock: 28},
@@ -49,10 +49,23 @@ export class ArticlesFamilyComponent implements OnInit {
   ];
 
   constructor(public articleService: ArticleService) {
-
+    this.getAllArticles();
   }
 
   ngOnInit() {
+
   }
 
+  getAllArticles() {
+    this.articleList = [];
+    this.articleService.readAll().subscribe(
+      data => {
+        console.log(data);
+        this.articleList = data;
+      },
+    );
+  }
 }
+
+
+
