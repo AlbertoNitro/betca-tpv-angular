79651e0ce2eab08b093546debd2b15950de43e03
Gabriel Munumel <gabrielmunumel@gmail.com>
2018-03-18 09:30:35 +0100

    Inicio Tarea 8

--- src/app/home/cash-movement/cash-movement.service.ts
+++ src/app/home/cash-movement/cash-movement.service.ts
@@ -10,11 +10,9 @@ export class CashMovementService {
 
     add(cashMovement: CashMovement): void {
         cashMovement.value = Math.abs(cashMovement.value);
-        // this will call a httpService and execute the add operation. Does not return anything
     }
 
     remove(cashMovement: CashMovement): void {
         cashMovement.value = (cashMovement.value < 0) ? cashMovement.value : -1 * cashMovement.value;
-        // this will call a httpService and execute the remove operation. Does not return anything
     }
 }
