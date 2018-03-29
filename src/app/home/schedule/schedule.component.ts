3255730247384ab11aaec0b9d60a3e6c1442c035
Jaime Hernandez <jaime.hernandezortiz@gmail.com>
2018-03-29 23:24:52 +0200

    Fin de tarea 2. Se unifica aspecto visual con el resto de la aplicación.

--- src/app/home/schedule/schedule.component.ts
+++ src/app/home/schedule/schedule.component.ts
@@ -11,14 +11,15 @@ export class ScheduleComponent implements OnInit {
     static URL = 'schedule';
     private schedulerList: Scheduler[] = [];
     dataSource: MatTableDataSource<Scheduler>;
-    displayedColumns = ['dateTime', 'title', 'description'];
+    displayedColumns = ['dateTime', 'title', 'description', 'actions'];
 
     constructor() {
       this.schedulerList.push({ dateTime: '01/01/2018 13:56', title: 'hola', description: 'Eoeo'});
+      this.schedulerList.push({ dateTime: '01/11/2018 08:56', title: 'eere', description: 'Eoeo'});
       this.dataSource = new MatTableDataSource<Scheduler>(this.schedulerList);
     }
 
     ngOnInit(): void {
-
     }
+
 }
