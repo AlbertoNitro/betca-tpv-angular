cf88d12505fc7c4d731e522ca85d9595e4a7433b
borjaguzmanlopez94 <borja.guzman.lopez@alumnos.upm.es>
2018-04-02 20:31:42 +0200

    Delete chart data test (Fin tarea 5)

--- src/app/home/statistics2/historicChart.component.ts
+++ src/app/home/statistics2/historicChart.component.ts
@@ -20,12 +20,9 @@ export class HistoricChartComponent implements OnInit {
     constructor(private ticketService: TicketService, private snackBar: MatSnackBar) { }
     // Atributos gráfica Historical products
     public lineChartData: Array<any> = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Producto A' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Producto B' },
-        { data: [18, 48, 77, 9, 100, 27, 40], label: 'Producto C' }
-
+        { data: [], label: '' },
     ];
-    public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartLabels: Array<any> = [''];
 
     public lineChartLegend = true;
     public lineChartType = 'line';
