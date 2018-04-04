f34c748b59693091fad759f0dcc23eb4d440a4ee
borjaguzmanlopez94 <borja.guzman.lopez@alumnos.upm.es>
2018-04-04 16:08:41 +0200

    Fin tarea 9 conexión entre el fron-end y el back-end para obtener los datos necesarios para la ultima gráfica

--- src/app/home/statistics2/incomeComparisonChart.component.ts
+++ src/app/home/statistics2/incomeComparisonChart.component.ts
@@ -2,7 +2,7 @@ import { OnInit, ViewChild, Component, SimpleChanges } from '@angular/core';
 import { MatTableDataSource, MatSnackBar } from '@angular/material';
 import { BaseChartDirective } from 'ng2-charts';
 import { FormControl } from '@angular/forms';
-
+import { TicketService } from '../shared/ticket.service';
 
 @Component({
     selector: 'app-income-comparison-chart',
@@ -16,7 +16,7 @@ export class IncomeComparisonChartComponent implements OnInit {
     @ViewChild('chartIncomeComparison') chartIncomeComparison: BaseChartDirective;
     initDate = new FormControl(new Date());
     endDate = new FormControl(new Date());
-    constructor(private snackBar: MatSnackBar) { }
+    constructor(private ticketService: TicketService, private snackBar: MatSnackBar) { }
     // Atributos gráfica percentage
     public disabled = false;
     public barChartLabels: string[] = ['product1', 'product2'];
@@ -34,18 +34,39 @@ export class IncomeComparisonChartComponent implements OnInit {
     refreshPercentageChart(): void {
         this.disabled = true;
         // hacer llamada al servicio
+        this.ticketService.readComprisionIncome(
+            new Date(this.initDate.value),
+            new Date(this.endDate.value)
+        ).subscribe(
+            data => {
+                if (data.length > 0) {
+                    const benefitData = [];
+                    const priceData = [];
+                    const auxLabel = [];
+                    this.barChartData.length = 0;
+                    for (const item of data) {
+                        auxLabel.push(item.productName);
+                        benefitData.push(item.income);
+                        priceData.push(item.productPrice);
+                    }
 
-        this.barChartLabels = ['product1', 'product2', 'product3', 'product4', 'product5', 'product6', 'product7'];
-        this.barChartData.length = 0;
-        const aux = [
-            { data: [65, 59, 80, 81, 56, 55, 40], label: this.benefitLiteral },
-            { data: [28, 48, 40, 19, 86, 27, 90], label: this.priceProductLiteral }
-        ];
-        for (const item of aux) {
-            this.barChartData.push(item);
-        }
+                    this.barChartData.push({ data: benefitData, label: this.benefitLiteral });
+                    this.barChartData.push({ data: priceData, label: this.priceProductLiteral });
+                    this.barChartLabels = auxLabel;
 
-        this.disabled = false;
+                } else {
+                    this.showError('No available data');
+                }
+            },
+            error => {
+                this.showError('Server not found, try again in a few minutes');
+                this.disabled = false;
+            },
+            () => {
+                this.disabled = false;
+            }
+        );
+
     }
 
     private showError(msg: string) {
