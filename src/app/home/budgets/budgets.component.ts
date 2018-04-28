486b550b05d5fbac6f1295cff95ae0bfef3c83ad
miw-upm <j.bernal@upm.es>
2018-04-28 22:57:49 +0200

    refactoring budgets

--- src/app/home/budgets/budgets.component.ts
+++ src/app/home/budgets/budgets.component.ts
@@ -1,34 +1,27 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import {Component} from '@angular/core';
+import {MatDialog} from '@angular/material';
 
-import { Budget } from '../shared/budget.model';
-import { BudgetService } from '../shared/budget.service';
+import {Budget} from '../shared/budget.model';
+import {BudgetService} from '../shared/budget.service';
 
 @Component({
-    templateUrl: 'budgets.component.html'
+  templateUrl: 'budgets.component.html'
 })
-export class BudgetsComponent implements OnInit {
-    static URL = 'budgets';
+export class BudgetsComponent {
+  static URL = 'budgets';
 
-    title = 'Budgets management';
-    columns = ['id'];
-    data: Budget[];
+  title = 'Budgets management';
+  columns = ['id'];
+  data: Budget[];
 
-    constructor(private dialog: MatDialog, private budgetService: BudgetService) {
-    }
+  constructor(private dialog: MatDialog, private budgetService: BudgetService) {
+    this.budgetService.readAll().subscribe(
+      data => this.data = data
+    );
+  }
 
-    ngOnInit(): void {
-        this.synchronize();
-    }
-
-    synchronize() {
-        this.budgetService.readAll().subscribe(
-            data => this.data = data
-        );
-    }
-
-    read(budget: Budget) {
-        this.budgetService.read(budget);
-    }
+  read(budget: Budget) {
+    this.budgetService.read(budget);
+  }
 
 }
