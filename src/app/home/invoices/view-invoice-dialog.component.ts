b79b557b87a6d8cb9191480bf02b4891ea455a4d
miw-upm <j.bernal@upm.es>
2018-04-28 20:20:11 +0200

    Refactoring invoice

--- src/app/home/invoices/view-invoice-dialog.component.ts
+++ src/app/home/invoices/view-invoice-dialog.component.ts
@@ -1,18 +1,16 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog } from '@angular/material';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
 
-import { Invoice } from '../shared/invoice.model';
-import { TicketService } from '../shared/ticket.service';
-import { EditTicketDialogComponent } from '../tickets/edit-ticket-dialog.component';
+import {Invoice} from '../shared/invoice.model';
 
 @Component({
-    templateUrl: './view-invoice-dialog.component.html'
+  templateUrl: './view-invoice-dialog.component.html'
 })
 export class ViewInvoiceDialogComponent {
 
-    invoice: Invoice;
+  invoice: Invoice;
 
-    constructor(@Inject(MAT_DIALOG_DATA) data: any) {
-        this.invoice = data.invoice;
-    }
+  constructor(@Inject(MAT_DIALOG_DATA) data: any) {
+    this.invoice = data.invoice;
+  }
 }
