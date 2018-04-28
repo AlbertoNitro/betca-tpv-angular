b0d287051f562510a5883fe8e1fe9937244ab7ec
miw-upm <j.bernal@upm.es>
2018-04-28 22:57:37 +0200

    refactoring admin

--- src/app/home/admin/admins.service.ts
+++ src/app/home/admin/admins.service.ts
@@ -1,53 +1,57 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
-import { HttpService } from '../../core/http.service';
-import { User } from '../shared/user.model';
+import {HttpService} from '../../core/http.service';
+import {User} from '../shared/user.model';
 
 @Injectable()
 export class AdminsService {
-    static END_POINT = '/admins';
-    static DB = '/db';
-    static ARTICLES_WITHOUT_CODE = '/articles-without-code';
-    static STATE = '/state';
-    static USERS = '/users';
+  static END_POINT = '/admins';
+  static DB = '/db';
+  static ARTICLES_WITHOUT_CODE = '/articles-without-code';
+  static STATE = '/state';
+  static USERS = '/users';
 
-    constructor(private httpService: HttpService) { }
-
-
-    powerOff() {
-        this.httpService.authToken().successful().delete(AdminsService.END_POINT + AdminsService.STATE).subscribe(
-            () => { }
-        );
-    }
-
-    seedDb(ymlFileName: string): void {
-        this.httpService.authToken().successful().post(AdminsService.END_POINT + AdminsService.DB, ymlFileName).subscribe(
-            () => { }
-        );
-    }
-
-    deleteDb(): void {
-        this.httpService.authToken().successful().delete(AdminsService.END_POINT + AdminsService.DB).subscribe(
-            () => this.httpService.logout()
-        );
-    }
-
-    resetDb(): void {
-        this.httpService.authToken().successful().delete(
-            AdminsService.END_POINT + AdminsService.DB + AdminsService.ARTICLES_WITHOUT_CODE).subscribe(
-                () => { }
-            );
-    }
+  constructor(private httpService: HttpService) {
+  }
 
 
-    readProfile(): Observable<User> {
-        return this.httpService.authToken().get(AdminsService.END_POINT + AdminsService.USERS + '/' + this.httpService.getMobile());
-    }
+  powerOff() {
+    this.httpService.authToken().successful().delete(AdminsService.END_POINT + AdminsService.STATE).subscribe(
+      () => {
+      }
+    );
+  }
 
-    updateProfile(user: User) {
-        return this.httpService.authToken().successful().put(
-            AdminsService.END_POINT + AdminsService.USERS + '/' + this.httpService.getMobile(), user);
-    }
+  seedDb(ymlFileName: string): void {
+    this.httpService.authToken().successful().post(AdminsService.END_POINT + AdminsService.DB, ymlFileName).subscribe(
+      () => {
+      }
+    );
+  }
+
+  deleteDb(): void {
+    this.httpService.authToken().successful().delete(AdminsService.END_POINT + AdminsService.DB).subscribe(
+      () => this.httpService.logout()
+    );
+  }
+
+  resetDb(): void {
+    this.httpService.authToken().successful().delete(
+      AdminsService.END_POINT + AdminsService.DB + AdminsService.ARTICLES_WITHOUT_CODE).subscribe(
+      () => {
+      }
+    );
+  }
+
+
+  readProfile(): Observable<User> {
+    return this.httpService.authToken().get(AdminsService.END_POINT + AdminsService.USERS + '/' + this.httpService.getMobile());
+  }
+
+  updateProfile(user: User) {
+    return this.httpService.authToken().successful().put(
+      AdminsService.END_POINT + AdminsService.USERS + '/' + this.httpService.getMobile(), user);
+  }
 
 }
